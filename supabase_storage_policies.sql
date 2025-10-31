59f9c8414dd14a31933ea3ddaa0ba9427cbcc79b
Usuario <usuario@example.com>
2025-10-31 12:00:00 -0500

    Commit for supabase_storage_policies.sql

--- supabase_storage_policies.sql
+++ supabase_storage_policies.sql
@@ -17,6 +17,8 @@ CREATE POLICY "Permitir ver adjuntos"
 ON storage.objects FOR SELECT 
 USING (bucket_id = 'adjuntos');
 
-CREATE POLICY "Permitir eliminar adjuntos" 
-ON storage.objects FOR DELETE 
+CREATE POLICY "Permitir eliminar adjuntos"
+ON storage.objects FOR DELETE
 USING (bucket_id = 'adjuntos');
+
+-- Commit diario 17
